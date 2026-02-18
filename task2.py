95f6bcf64c60f36dc4f65663d3741fbe465db6c7
kamila-velazquez533 <kamila.velazquez533@students.cmcss.net>
2026-02-18 12:33:41 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -7,34 +7,34 @@ from data import first, last, birth, days, p_first, p_last, phone, street, city,
 
 
 # first
-
+first = input("Child's Firts Name: ")
 
 # last
-
+last = input("Child's Last Name: ")
 
 # birth
-
+ birth = input("In what year was {first} {last} born: ")
 
 # days
-
+ days = input("How many days will {first} attend: ")
 
 # p_first
-
+p_first = input("Parent's First Name: ")
 
 # p_last
-
+p_last = input("Parent's Last Name: ")
 
 # phone
-
+ phone = input("Parent's Phone #: ")
 
 # street
-
+street = input("Street Address: ")
 
 # city
-
+ city = input("City: ")
 
 # state
-
+state = input("Street Abbreviation: ")
 
 # zip_code
-
+zip = input("Zip code: ")
