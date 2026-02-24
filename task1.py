6279ca9b98e2e2dc3ad89288f0b599e6692c8abb
kamila-velazquez533 <kamila.velazquez533@students.cmcss.net>
2026-02-24 11:09:39 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -7,11 +7,11 @@ def main():
     print(f"Camper's Name: {first} {last}")
 
     #Variable: birth Prompt: In what year was {first} {last} born:
-    birth - input("In what year was {first} {last} born: ")
+    birth - input(f"In what year was {first} {last} born: ")
     print(f"Birth Year: {birth}")
 
     #Variable: days Prompt: How many days will {first} attend?
-    days = input("How many days will {first) attend: ")
+    days = input(f"How many days will {first) attend: ")
     print(f"Camp Duration: {days} days")
 
     #Variable: p_first Prompt: Parent's First Name:
@@ -29,7 +29,7 @@ def main():
     #Variable: city Prompt: City:
     city = input("City: ")
     #Variable: state Prompt: State Abbreviation:
-    state = input("Street Abbreviation: ")
+    state = input("State Abbreviation: ")
     #Variable: zip Prompt: Zip Code:
     zip = input("Zip code: ")
     print(f"Address:\n{street}\n{city}, {state} {zip}")
